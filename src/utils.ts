34d4b55b89304c5e0346ded596b1a51ad083a633
Lidan Turkov <lidan.delta@gmail.com>
2025-04-17 16:48:11 +0300

    fix: js format

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-import { Client, Chat } from "whatsapp-web.js";
-
-export const findGroupByName = async (
-  client: Client,
-  groupName: string
-): Promise<Chat | undefined> => {
-  const chats = await client.getChats();
-  return chats.find((chat) => chat.name.includes(groupName));
-};
